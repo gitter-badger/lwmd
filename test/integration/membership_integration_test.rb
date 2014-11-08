74fa3b3bad281d0ca102bbfb477ef3443802b238
Jim Wrubel <jwrubel@gmail.com>
2014-11-08 18:34:20 -0500

    Integration tests for memberships. [#81871336]

--- test/integration/membership_integration_test.rb
+++ test/integration/membership_integration_test.rb
@@ -1,25 +1,72 @@
 require 'test_helper'
 
-class MemberIntegrationTest < IntegrationTest
+class MembershipIntegrationTest < IntegrationTest
   before do
 
   end
 
-  it "can view a list of members" do
-    @member = create(:member)
-    visit members_path
-    must_have_content @member.name
+  it "can view a list of memberships" do
+    im = build(:individual_membership)
+    mm = build(:member_membership, member: create(:member), primary: true)
+    im.member_memberships << mm
+    im.save
+    primary_member = create(:member)
+    fm = build(:family_membership)
+    pmm = build(:member_membership, member: primary_member, primary: true)
+    fm.member_memberships << pmm
+    3.times do
+      family_member = create(:member)
+      fmm = build(:member_membership, member: family_member)
+      fm.member_memberships << fmm
+    end
+    fm.save
+    visit memberships_path
+    must_have_content im.primary_member.name
+    fm.members.each do |m|
+      must_have_content m.name
+    end
   end
 
-  it "adds a member" do
-
+  it "adds an individual membership" do
+    member = create(:member)
+    visit new_membership_path
+    within("form.new_membership") do
+      fill_in 'Price paid', :with => 'First'
+      fill_in 'Year', :with => 'Last'
+      fill_in 'Date paid', :with => Date.today
+      find("input#membership_0").set(member.id)
+    end
+    click_button 'Add Membership'
+    page.must_have_css('.ui.blue.message.closable')
   end
 
-  it "wont add a member with missing fields" do
-
+  it "adds a family membership" do
+    primary_member = create(:member)
+    family_member = create(:member)
+    visit new_membership_path(category: "Family")
+    within("form.new_membership") do
+      fill_in 'Price paid', :with => '$40'
+      fill_in 'Year', :with =>  Date.today.year
+      fill_in 'Date paid', :with => Date.today
+      find("input#membership_0").set(primary_member.id)
+      find("input#membership_1").set(family_member.id)
+    end
+    click_button 'Add Membership'
+    page.must_have_css('.ui.blue.message.closable')
   end
 
-  it "wont add a membership without at least one member" do
-
+  it "wont add a membership with missing fields" do
+    member = create(:member)
+    visit new_membership_path
+    within("form.new_membership") do
+      #nothing - just click the button
+    end
+    click_button 'Add Membership'
+    page.wont_have_css('.ui.blue.message.closable')
+    page.must_have_css('.ui.red.message')
+    page.must_have_content("Price paid can't be blank")
+    page.must_have_content("Year can't be blank")
+    page.must_have_content("Date paid can't be blank")
+    page.must_have_content("Members You need at least one member")
   end
 end
--- test/unit/member_membership_test.rb
+++ test/unit/member_membership_test.rb
@@ -6,11 +6,13 @@ class MemberMembershipTest < UnitTest
 
     it "doesnt allow a member to be added to more than one membership per year" do
       member = create(:member)
-      first_membership = build(:membership, year: Date.today.year)
-      first_membership.members << member
+      first_membership = build(:individual_membership, year: Date.today.year)
+      mm1 = build(:member_membership, member: member)
+      first_membership.member_memberships << mm1
       first_membership.save.must_equal true
-      second_membership = build(:membership, year: Date.today.year)
-      second_membership.members << member
+      second_membership = build(:individual_membership, year: Date.today.year)
+      mm2 = build(:member_membership, member: member)
+      second_membership.member_memberships << mm2
       second_membership.save.must_equal false
     end
 
