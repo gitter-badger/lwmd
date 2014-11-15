03dcaab7b2e1f1c5bf77cc06969a08980045596a
Jim Wrubel <jwrubel@gmail.com>
2014-11-15 10:40:15 -0500

    Remove double flash messages by moving it to the template. [#82470766]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     case level
       when "success" then "ui green message"
       when "error" then "ui red message"
-      when "notice" then "ui blue message"
+      else "ui blue message"
     end
   end
 end
