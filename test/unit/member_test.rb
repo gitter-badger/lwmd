8d52c9e2188e0766bb1d572f02ab5aaa9b29f5cb
Jim Wrubel <jwrubel@gmail.com>
2014-11-17 10:02:03 -0500

    unit tests for member profile methods [#82812350]

--- test/unit/member_test.rb
+++ test/unit/member_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class MemberTest < UnitTest
 
-  describe "valid member data" do
+  describe "a member" do
 
     it "should save valid member data" do
       member = create(:member)
@@ -18,6 +18,38 @@ class MemberTest < UnitTest
     it "gets a list of members" do
       2.times{ create(:member) }
       Member.all.length.must_equal 2
+    end
+
+    it "makes a valid member number" do
+      member = create(:member)
+      member.member_number.must_equal Integer(Date.today.year.to_s \
+        + (member.id + Member::MEMBER_SEED).to_s.rjust(5, "0"))
+    end
+
+    it "knows its membership status" do
+      member = create(:member)
+      this_year = Date.today.year
+      last_year = this_year-1
+      im = build(:individual_membership, year: last_year)
+      mm = build(:member_membership, member: member, primary: true)
+      im.member_memberships << mm
+      im.save
+      member.paid_up?.must_equal false
+      member.lapsed?.must_equal true
+      im2 = build(:individual_membership, year: this_year)
+      mm2 = build(:member_membership, member: member, primary: true)
+      im2.member_memberships << mm2
+      im2.save
+      member.paid_up?.must_equal true
+      member.lapsed?.must_equal false
+      member.since.must_equal last_year
+    end
+
+    it "knows its role" do
+      admin = create(:admin)
+      member = create(:member)
+      admin.role.must_equal "admin"
+      member.role.must_equal "member"
     end
 
   end
