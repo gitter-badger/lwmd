bd0e41c840a646451ded13b820e2b7e57628ae9c
Jim Wrubel <jwrubel@gmail.com>
2014-11-26 13:50:23 -0500

    Adjust asset settings to match recommendations from heroku_rails_deflate gem. [#83452706]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,22 +51,26 @@ Rails.application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+  config.assets.enabled = true
+
   config.action_controller.asset_host = ENV['ASSET_HOST'] || "//images.pittsburghtriathlonclub.com"
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
+
+  config.assets.compress = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
 
-  config.assets.enabled = true
+
 
   config.assets.initialize_on_precompile = true
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
