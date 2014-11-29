15efe971a67489281a1521b0d99ffc847a3962b9
Jim Wrubel <jwrubel@gmail.com>
2014-11-29 10:06:26 -0500

    update callbacks for google.  [#82382942]

--- app/controllers/members/omniauth_callbacks_controller.rb
+++ app/controllers/members/omniauth_callbacks_controller.rb
@@ -4,15 +4,29 @@ class Members::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # the connection for the first time
     auth = request.env["omniauth.auth"]
 
-    # not getting provider or ui
-    logger.info auth.inspect
-    
     token = cookies[:token]
     @member = Member.from_omniauth(auth, token)
     cookies.delete :token
     if @member.persisted?
       sign_in_and_redirect @member, :event => :authentication
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+    else
+      flash[:error] = "Sorry! We could not connect you with any member account."
+      redirect_to unauthenticated_root_path
+    end
+  end
+
+  def google
+    # invitation_token will be included if the member is setting up
+    # the connection for the first time
+    auth = request.env["omniauth.auth"]
+
+    token = cookies[:token]
+    @member = Member.from_omniauth(auth, token)
+    cookies.delete :token
+    if @member.persisted?
+      sign_in_and_redirect @member, :event => :authentication
+      set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
     else
       flash[:error] = "Sorry! We could not connect you with any member account."
       redirect_to unauthenticated_root_path
